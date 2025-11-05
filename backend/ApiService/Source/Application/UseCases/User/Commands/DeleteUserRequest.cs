7d4ddedbd621a43b52076e129e25895625563434
Igor Perekrestov <perekrestov.igor.p@gmail.com>
2025-11-05 10:19:27 +0200

    fix

--- backend/ApiService/Source/Application/UseCases/User/Commands/DeleteUserRequest.cs
+++ backend/ApiService/Source/Application/UseCases/User/Commands/DeleteUserRequest.cs
@@ -3,9 +3,8 @@ using FluentValidation.Results;
 using MediatR;
 using RoomAggregate = Epam.ItMarathon.ApiService.Domain.Aggregate.Room.Room;
 
-namespace Epam.ItMarathon.ApiService.Application.UseCases.User.Queries
+namespace Epam.ItMarathon.ApiService.Application.UseCases.User.Commands
 {
-/// 
     public record DeleteUserRequest(string UserCode, ulong? UserId)
         : IRequest<Result<RoomAggregate, ValidationResult>>;
 }
--- backend/ApiService/Source/Application/UseCases/User/Handlers/DeleteUserHandler.cs
+++ backend/ApiService/Source/Application/UseCases/User/Handlers/DeleteUserHandler.cs
@@ -1,6 +1,6 @@
 ﻿using System.Security.Cryptography.X509Certificates;
 using CSharpFunctionalExtensions;
-using Epam.ItMarathon.ApiService.Application.UseCases.User.Queries;
+using Epam.ItMarathon.ApiService.Application.UseCases.User.Commands;
 using Epam.ItMarathon.ApiService.Domain.Abstract;
 using Epam.ItMarathon.ApiService.Domain.Shared.ValidationErrors;
 using FluentValidation.Results;
@@ -43,43 +43,6 @@ namespace Epam.ItMarathon.ApiService.Application.UseCases.User.Handlers
             //4. Get updated room
             var updatedRoomResult = await roomRepository.GetByUserCodeAsync(request.UserCode, cancellationToken);
             return updatedRoomResult;
-
-
-
-
-
-
-            // var authUserResult = await userRepository.GetByCodeAsync(request.UserCode, cancellationToken,
-            //     includeRoom: true, includeWishes: true);
-            // if (authUserResult.IsFailure)
-            // {
-            //     return authUserResult.ConvertFailure<List<UserEntity>>();
-            // }
-            //
-            // if (request.UserId is null)
-            // {
-            //     // Get all users in room
-            //     var roomId = authUserResult.Value.RoomId;
-            //     var result = await userRepository.GetManyByRoomIdAsync(roomId, cancellationToken);
-            //     return result;
-            // }
-            //
-            // // Otherwise, Get user by id
-            // var requestedUserResult = await userRepository.GetByIdAsync(request.UserId.Value, cancellationToken,
-            //     includeRoom: false, includeWishes: true);
-            // if (requestedUserResult.IsFailure)
-            // {
-            //     return requestedUserResult.ConvertFailure<List<UserEntity>>();
-            // }
-            //
-            // if (requestedUserResult.Value.RoomId != authUserResult.Value.RoomId)
-            // {
-            //     return Result.Failure<List<UserEntity>, ValidationResult>(new NotAuthorizedError([
-            //         new ValidationFailure("id", "User with userCode and user with Id belongs to different rooms.")
-            //     ]));
-            // }
-            //
-            // return new List<UserEntity> { requestedUserResult.Value, authUserResult.Value };
         }
     }
 }
