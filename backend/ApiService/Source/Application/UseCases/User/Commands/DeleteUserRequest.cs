f0e2f85d5f3e52bac8c51d3b9874b33dbe8fdce5
Igor Perekrestov <perekrestov.igor.p@gmail.com>
2025-11-05 12:15:08 +0200

    new delete VsCode

---
+++ backend/ApiService/Source/Application/UseCases/User/Commands/DeleteUserRequest.cs
@@ -0,0 +1,11 @@
+﻿using CSharpFunctionalExtensions;
+using FluentValidation.Results;
+using MediatR;
+using RoomAggregate = Epam.ItMarathon.ApiService.Domain.Aggregate.Room.Room;
+
+namespace Epam.ItMarathon.ApiService.Application.UseCases.User.Commands
+{
+    /// <summary>
+     public record DeleteUserRequest(string UserCode, ulong? UserId)
+        : IRequest<Result<RoomAggregate, ValidationResult>>;
+}
